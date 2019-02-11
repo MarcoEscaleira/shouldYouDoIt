62a16cf3220676786d21f8e9d042d206627807b1
MarcoEscaleira <marco.escaleira@school.mindera.com>
2019-02-11 16:35:25 +0000

    Flexbox for modal

--- src/components/ErrorModal.js
+++ src/components/ErrorModal.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 
 const ErrorModal = ({ show, text, closeCallback}) => (
   <div className={`modal ${!show ? 'modal--invisible' : ''}`}>
-    <div className="modal__overlay" onClick={closeCallback}></div>
-    <div className="modal__content">
-      <h3 className="modal__title">Error</h3>
-      <p className="modal__body">{text}</p>
-      <button 
-        className="modal__button"
-        onClick={closeCallback}
-      > 
-        Okay
-      </button>
+    <div className="modal__overlay" onClick={closeCallback}>
+      <div className="modal__content">
+        <h3 className="modal__title">Error</h3>
+        <p className="modal__body">{text}</p>
+        <button 
+          className="modal__button"
+          onClick={closeCallback}
+        > 
+          Okay
+        </button>
+      </div>
     </div>
   </div>
 );
