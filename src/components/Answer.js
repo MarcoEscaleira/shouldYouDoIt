5809889a75ac4d268827077fee141ebaae859303
MarcoEscaleira <marco.escaleira@school.mindera.com>
2019-02-12 23:02:51 +0000

    Handling response msg
    Making response msg first letter uppercase and rest lower

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
+const handleMsg = (str) => {
+    return str.toLowerCase().replace(/^[a-z]{1}/igm, (m) => { return m.toUpperCase() });
+};
+
 const Answer = ({handleData, data: {msg, img}}) => (
   <div className="answer">
-    <h1 className="answer__title">{ (msg === 'loading') ? "Loading" : msg }</h1>
+    <h1 className="answer__title">{ (msg === 'loading') ? "Loading" : handleMsg(msg) }</h1>
     <img 
       src={img}
-      alt="yesOrNoApiImage"
+      alt="Should You Do It image"
       className="answer__image"
     />
     <button 
