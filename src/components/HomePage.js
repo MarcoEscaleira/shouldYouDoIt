2738f0a9a779706e3862b01f3018c83dc7dc6c45
Marco Escaleira <marcoescaleira2000@gmail.com>
2019-02-18 14:30:07 +0000

    InputRegex

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -81,7 +81,9 @@ class Home extends Component {
 
   handleTextInput = (e) => {
     const textInput = e.target.value;
-    this.setState(() => ({ textInput }));
+    if(!textInput || textInput.match(/^[a-z0-9 ]+$/i)) {
+      this.setState(() => ({ textInput }));
+    }
   };
 
   render() {
