fe6d3a2a40fead957eb7e3e5fbd15b084d1b7931
Marco Escaleira <marcoescaleira2000@gmail.com>
2019-05-18 12:58:13 +0100

    Hide redux devtools in production

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,15 @@
-import { createStore, applyMiddleware, compose} from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
 
-import decisionReducer from './decisions'
+import decisionReducer from "./decisions";
 
 export default () => {
   const middlewares = [thunk];
-  const enhancers = [
-    applyMiddleware(...middlewares)
-  ];
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  
+  const enhancers = [applyMiddleware(...middlewares)];
+  const composeEnhancers =
+    process.env.NODE_ENV === "production"
+      ? compose
+      : window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
   return createStore(decisionReducer, composeEnhancers(...enhancers));
 };
