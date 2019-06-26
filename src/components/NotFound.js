bbb023b5efc328136e3d799e6d5d28ffcc2ecdda
Marco Escaleira <marcoescaleira2000@gmail.com>
2019-06-26 17:37:20 +0100

    404 Page styled

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-  <div>
-    404 - <Link to="/">Go Home</Link>
+  <div className="notFound">
+    <p className="notFound__label">404</p>
+    <p className="notFound__text">
+      Oops, we can't seem to find the page you're looking for.
+    </p>
+    <Link to="/" className="notFound__link">
+      Go Home
+    </Link>
   </div>
 );
 
